fd3484bffe425d73bd3e4a4093cf6e66e14b4558
Krzysztof Hankiewicz @kjhank <kjhank@gmail.com>
2023-07-11 17:45:10 +0200

    updated HTML chapter examples

---
+++ globals/js/page.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const currentPathname = window.location.pathname.split('/').at(-1);
+  const navNodes = Array.from(document.querySelectorAll('.main-navigation__item'));
+
+  navNodes.forEach(element => {
+    const isCurrentLink = element.querySelector(`a[href*="${currentPathname}"]`);
+
+    if (isCurrentLink) {
+      element.classList.add('main-navigation__item--current');
+    }
+  });
+
+});
