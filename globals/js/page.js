60879c97b1fdc78bbf3b381a098fb928b6d8e508
Krzysztof Hankiewicz @kjhank <kjhank@gmail.com>
2023-08-17 16:06:19 +0200

    add table of contents to examples

--- globals/js/page.js
+++ globals/js/page.js
@@ -1,3 +1,54 @@
+import { paths } from './paths.js';
+
+const getPaths = () => {
+  return paths.reduce((result, current) => {
+    const isIndex = current.path.endsWith('index.html');
+
+    if (isIndex) {
+      return [...result, current];
+    }
+
+    return result;
+
+  }, []);
+}
+
+const renderPaths = () => {
+  const indices = getPaths();
+
+  const innerHTML = indices.reduce((result, current) => {
+    const liString = `<li><a href="${current.path}">${current.name}</a></li>`;
+
+    return `${result}${liString}`;
+  }, '');
+
+
+  return innerHTML;
+}
+
+const attachPathsToNav = () => {
+  const mainNav = document.querySelector('.main-navigation');
+
+  const pathsHTML = renderPaths();
+  const tocButton = document.createElement('button');
+  const toc = document.createElement('ul');
+
+  const handleButtonClick = () => {
+    toc.classList.toggle('main-navigation__toc--active');
+  }
+
+  toc.classList.add('main-navigation__toc');
+  tocButton.classList.add('main-navigation__toc-toggle');
+  tocButton.innerText = 'Spis treści';
+  toc.innerHTML = pathsHTML;
+
+  mainNav.appendChild(toc);
+  mainNav.appendChild(tocButton);
+
+  tocButton.addEventListener('click', handleButtonClick);
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const currentPathname = window.location.pathname.split('/').at(-1);
   const navNodes = Array.from(document.querySelectorAll('.main-navigation__item'));
@@ -10,4 +61,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  attachPathsToNav();
 });
---
+++ globals/js/paths.js
@@ -0,0 +1,274 @@
+export const paths = [
+  {
+    name: 'SVG: elementy',
+    path: '/modules/4.8_SVG/index.html'
+  },
+  {
+    name: 'SVG: pliki',
+    path: '/modules/4.8_SVG/files.html'
+  },
+  {
+    name: 'SVG: stylizacja',
+    path: '/modules/4.8_SVG/styling.html'
+  },
+  {
+    name: 'SVG: użycie',
+    path: '/modules/4.8_SVG/usage.html'
+  },
+  {
+    name: 'Flexbox: wstęp',
+    path: '/modules/3.6_flexbox/index.html'
+  },
+  {
+    name: 'Flexbox: kontener',
+    path: '/modules/3.6_flexbox/flex-container.html'
+  },
+  {
+    name: 'Flexbox: elementy',
+    path: '/modules/3.6_flexbox/flex-items.html'
+  },
+  {
+    name: 'BEM: bloki, elementy, modyfikatory',
+    path: '/modules/7.13_BEM/index.html'
+  },
+  {
+    name: 'BEM: przykłady',
+    path: '/modules/7.13_BEM/homework-6.html'
+  },
+  {
+    name: 'RWD: media queries',
+    path: '/modules/8.15_RWD/index.html'
+  },
+  {
+    name: 'RWD: `viewport`',
+    path: '/modules/8.15_RWD/meta-viewport.html'
+  },
+  {
+    name: 'Tranzyje, animacje: przejścia',
+    path: '/modules/5.10_transitions-animations/index.html'
+  },
+  {
+    name: 'Tranzyje, animacje: transformacje',
+    path: '/modules/5.10_transitions-animations/2d-transforms.html'
+  },
+  {
+    name: 'Tranzyje, animacje: perspektywa',
+    path: '/modules/5.10_transitions-animations/perspective.html'
+  },
+  {
+    name: 'Tranzyje, animacje: animacje',
+    path: '/modules/5.10_transitions-animations/animations.html'
+  },
+  {
+    name: 'RWD: grafiki',
+    path: '/modules/8.16_responsive-images/index.html'
+  },
+  {
+    name: 'RWD: `picture` oraz tła',
+    path: '/modules/8.16_responsive-images/picture-backgrounds.html'
+  },
+  {
+    name: 'RWD: przykłady',
+    path: '/modules/8.16_responsive-images/hw-example.html'
+  },
+  {
+    name: 'Pozycjonowanie: wstęp',
+    path: '/modules/5.9_positioning/index.html'
+  },
+  {
+    name: 'Pozycjonowanie: `overflow`',
+    path: '/modules/5.9_positioning/overflow.html'
+  },
+  {
+    name: 'Pozycjonowanie: konteksty warstwowe i `z-index`',
+    path: '/modules/5.9_positioning/z-index-stacking.html'
+  },
+  {
+    name: 'HTML: dostępność',
+    path: '/modules/1.1-2_HTML/accessibility.html'
+  },
+  {
+    name: 'HTML: struktura dokumentu',
+    path: '/modules/1.1-2_HTML/document-structure.html'
+  },
+  {
+    name: 'HTML: `div` udający button',
+    path: '/modules/1.1-2_HTML/faux-button.html'
+  },
+  {
+    name: 'HTML: tagi (znaczniki)',
+    path: '/modules/1.1-2_HTML/index.html'
+  },
+  {
+    name: 'HTML: link z obrazkiem',
+    path: '/modules/1.1-2_HTML/image-link.html'
+  },
+  {
+    name: 'HTML: elementy interaktywne: linki i przyciski',
+    path: '/modules/1.1-2_HTML/links-buttons.html'
+  },
+  {
+    name: 'HTML: elementy tekstowe',
+    path: '/modules/1.1-2_HTML/text-tags.html'
+  },
+  {
+    name: 'HTML: multimedia',
+    path: '/modules/1.1-2_HTML/multimedia.html'
+  },
+  {
+    name: 'HTML: listy',
+    path: '/modules/1.1-2_HTML/lists.html'
+  },
+  {
+    name: 'HTML: przycisk',
+    path: '/modules/1.1-2_HTML/real-button.html'
+  },
+  {
+    name: 'HTML: tabele',
+    path: '/modules/1.1-2_HTML/tables.html'
+  },
+  {
+    name: 'HTML: obrazy',
+    path: '/modules/1.1-2_HTML/images.html'
+  },
+  {
+    name: 'HTML: linki',
+    path: '/modules/1.1-2_HTML/links.html'
+  },
+  {
+    name: 'HTML: znaki specjalne',
+    path: '/modules/1.1-2_HTML/special-chars.html'
+  },
+  {
+    name: 'HTML: elementy tekstowe: cd.',
+    path: '/modules/1.1-2_HTML/text-ctd.html'
+  },
+  {
+    name: 'Webfonts: dodatkowe czcionki',
+    path: '/modules/2.4_text-fonts/web-fonts.html'
+  },
+  {
+    name: 'Tekst i czcionki',
+    path: '/modules/2.4_text-fonts/index.html'
+  },
+  {
+    name: 'Pseudoelementy tekstowe',
+    path: '/modules/2.4_text-fonts/text-pseudoelements.html'
+  },
+  {
+    name: 'Właściwości czcionek',
+    path: '/modules/2.4_text-fonts/font-properties.html'
+  },
+  {
+    name: 'SASS: intro',
+    path: '/modules/7.14_SASS/index.html'
+  },
+  {
+    name: 'SASS: `import` i `extend`',
+    path: '/modules/7.14_SASS/imports-extends.html'
+  },
+  {
+    name: 'SASS: mixiny',
+    path: '/modules/7.14_SASS/mixins.html'
+  },
+  {
+    name: 'SASS: `each` i mapy',
+    path: '/modules/7.14_SASS/each-maps.html'
+  },
+  {
+    name: 'SASS: arytmetyka',
+    path: '/modules/7.14_SASS/maths.html'
+  },
+  {
+    name: 'Tła elementów',
+    path: '/modules/4.7_decorative/index.html'
+  },
+  {
+    name: 'Cienie',
+    path: '/modules/4.7_decorative/shadows.html'
+  },
+  {
+    name: 'Dopasowanie obrazu w elemencie',
+    path: '/modules/4.7_decorative/object-fit-position.html'
+  },
+  {
+    name: 'Pseudoelementy',
+    path: '/modules/4.7_decorative/pseudos.html'
+  },
+  {
+    name: 'Model blokowy',
+    path: '/modules/3.5_box-model/index.html'
+  },
+  {
+    name: 'Pseudoelementy strukturalne',
+    path: '/modules/3.5_box-model/structural-pseudos.html'
+  },
+  {
+    name: 'Typy elementów',
+    path: '/modules/3.5_box-model/element-types.html'
+  },
+  {
+    name: 'Geometria elementu',
+    path: '/modules/3.5_box-model/geometry.html'
+  },
+  {
+    name: 'Kolory',
+    path: '/modules/2.3_selectors-colors/colors.html'
+  },
+  {
+    name: 'CSS: wprowadzenie',
+    path: '/modules/2.3_selectors-colors/index.html'
+  },
+  {
+    name: 'Zmienne (custom properties)',
+    path: '/modules/2.3_selectors-colors/variables.html'
+  },
+  {
+    name: 'Pseudoklasy stanowe',
+    path: '/modules/2.3_selectors-colors/pseudo-classes.html'
+  },
+  {
+    name: 'Selektory',
+    path: '/modules/2.3_selectors-colors/selectors.html'
+  },
+  {
+    name: 'Kaskada',
+    path: '/modules/2.3_selectors-colors/cascade.html'
+  },
+  {
+    name: 'Elementy formularzy',
+    path: '/modules/6.11_form-elements/select.html'
+  },
+  {
+    name: 'Etykieta: `label`',
+    path: '/modules/6.11_form-elements/label.html'
+  },
+  {
+    name: 'Formularze',
+    path: '/modules/6.11_form-elements/index.html'
+  },
+  {
+    name: 'Element podpowiedzi: `datalist`',
+    path: '/modules/6.11_form-elements/datalist.html'
+  },
+  {
+    name: 'Tekst wielowierszowy: `textarea`',
+    path: '/modules/6.11_form-elements/textarea.html'
+  },
+  {
+    name: 'Pole wejściowe: `input`',
+    path: '/modules/6.11_form-elements/input.html'
+  },
+  {
+    name: 'Grupowanie elementów',
+    path: '/modules/6.11_form-elements/grouping.html'
+  },
+  {
+    name: 'Pseudoklasy elementów formularza',
+    path: '/modules/6.12_form-validation/form-pseudoclasses.html'
+  },
+  {
+    name: 'Walidacja formularza po stronie klienta',
+    path: '/modules/6.12_form-validation/index.html'
+  },
+];
